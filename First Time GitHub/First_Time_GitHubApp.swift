20209157f8e9396dde7ffebe5c37f8540f7e1f56
Tenealle <tenealleslo@gmail.com>
2023-07-14 09:17:57 -0500

    Initial Commit

---
+++ First Time GitHub/First_Time_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_Time_GitHubApp.swift
+//  First Time GitHub
+//
+//  Created by Angela Sloan on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct First_Time_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
